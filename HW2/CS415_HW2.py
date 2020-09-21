48d85f69090a38fa1c4058e5cff4cff6223b3303
xliu225 <xliu225@uic.edu>
2020-09-21 18:15:00 -0500

    Gradient calculation added

--- HW2/CS415_HW2.py
+++ HW2/CS415_HW2.py
@@ -1,10 +1,10 @@
 """
 Xichen Liu
 CS415
-Canny edge detector on gray pics
+Canny edge detector
 """
 
-import cv2
+from scipy import ndimage
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -36,32 +36,59 @@ def GaussianSmooth(img, kernel_size, sigma):
 
     kernel = kernel / sum_val
 
-    # Get the length and width of image
-    i_rows = len(img)
-    i_cols = len(img[0])
+    # Implement the Convolution filter manually
+    # # Get the length and width of image
+    # i_rows = len(img)
+    # i_cols = len(img[0])
+    #
+    # # flip the kernel
+    # for i in range(kernel_size // 2):
+    #     kernel[i], kernel[kernel_size - i - 1] = kernel[kernel_size - 1 - i], kernel[i]
+    # for j in range(kernel_size):
+    #     for k in range(kernel_size // 2):
+    #         kernel[j][k], kernel[j][kernel_size - k - 1] = kernel[j][kernel_size - k - 1], kernel[j][k]
+    #
+    # # implement the pad of the boarder of image
+    # pad = (kernel_size - 1) // 2
+    # img = cv2.copyMakeBorder(img, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
+    #
+    # # Initialized output image
+    # dst = np.zeros((i_rows, i_cols), dtype = "float32")
+    # # get the pad rid of the desired output
+    # for i in np.arange(pad, i_rows + pad):
+    #     for j in np.arange(pad, i_cols + pad):
+    #         # The area from original image which is applied by the kernel
+    #         sample_from_img = img[i - pad: i + pad + 1, j - pad: j + pad + 1]
+    #         k = (sample_from_img * kernel).sum()
+    #         dst[i - pad, j - pad] = k
 
-    # flip the kernel
-    for i in range(kernel_size // 2):
-        kernel[i], kernel[kernel_size - i - 1] = kernel[kernel_size - 1 - i], kernel[i]
-    for j in range(kernel_size):
-        for k in range(kernel_size // 2):
-            kernel[j][k], kernel[j][kernel_size - k - 1] = kernel[j][kernel_size - k - 1], kernel[j][k]
-
-    # implement the pad of the boarder of image
-    pad = (kernel_size - 1) // 2
-    img = cv2.copyMakeBorder(img, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
-
-    # Initialized output image
-    dst = np.zeros((i_rows, i_cols), dtype = "float32")
-    # get the pad rid of the desired output
-    for i in np.arange(pad, i_rows + pad):
-        for j in np.arange(pad, i_cols + pad):
-            # The area from original image which is applied by the kernel
-            sample_from_img = img[i - pad: i + pad + 1, j - pad: j + pad + 1]
-            k = (sample_from_img * kernel).sum()
-            dst[i - pad, j - pad] = k
+    dst = ndimage.filters.convolve(img, kernel)
 
     return dst
+
+
+def ImageGradient(img):
+    # Build the Sobel filter of x axis and y axis
+    sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    sobel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
+
+    # Apply the sobel filter to img
+    img_x = ndimage.filters.convolve(img, sobel_x)
+    img_y = ndimage.filters.convolve(img, sobel_y)
+    plt.figure()
+    plt.imshow(img_x, cmap = 'gray')
+    plt.title('X direction gradient')
+    plt.show()
+    plt.figure()
+    plt.imshow(img_y, cmap = 'gray')
+    plt.title('Y direction gradient')
+    plt.show()
+
+    # Apply the formula, Consider gradient of x direction and y direction as wwo right-angled edges
+    Mag = np.hypot(img_x, img_y)
+    Theta = np.arctan2(img_x, img_y)
+
+    return Mag, Theta
 
 
 def main():
@@ -74,9 +101,16 @@ def main():
 
     kernel_size = int(input('Enter the kernel edges\' length: '))
     sigma = int(input('Enter value of sigma: '))
-    GaussianSmoothed = GaussianSmooth(img, kernel_size, sigma)
-    # plt.imshow(GaussianSmoothed, cmap = 'gray')
-    # plt.show()
+    GaussianSmoothed = GaussianSmooth(img_2, kernel_size, sigma)
+    Mag, Theta = ImageGradient(GaussianSmoothed)
+    plt.figure()
+    plt.imshow(Mag, cmap = 'gray')
+    plt.title('Gradient Magnitude')
+    plt.show()
+    plt.figure()
+    plt.imshow(Theta, cmap = 'gray')
+    plt.title('Gradient Direction')
+    plt.show()
 
 
 if __name__ == '__main__':
