1ea3a8a992ba81aa4d86c22aa8eea0dc7f9b8c45
xliu225 <xliu225@uic.edu>
2020-11-10 00:12:06 -0600

    Step 2, 3 should be done, implementing knn

--- HW5/CS415_HW5.py
+++ HW5/CS415_HW5.py
@@ -7,46 +7,109 @@ Image classification
 import os
 import sys
 import cv2
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
 import numpy as np
 
 dir_path = os.path.dirname((os.path.realpath(__file__)))
 sys.path.insert(0, dir_path)
 
+class Descriptor:
+    def __init__(self, vector = None, label = None):
+        self.vector = vector
+        self.label = label
 
-def sift_detector(img):
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # print(img_gray.shape)
+class Image:
+    def __init__(self, img = None, name = None, key_p = None, des = None, hist = None):
+        self.image = img
+        self.image_name = name
+        self.key_point = key_p
+        self.descriptor = des
+        self.label = None
+        self.histogram = hist
 
+class KnnClassifier:
+    def __init__(self, k = 1):
+        self.k = k
+
+    def predict(self, train_imgs, test_img):
+
+
+
+
+def update_images(imgs):
+    key_des = []
     sift = cv2.SIFT_create()
-    kp, des = sift.detectAndCompute(img_gray, None)
-    cv2.drawKeypoints(img_gray, kp, img, flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    for i in range(len(imgs)):
+        img_gray = cv2.cvtColor(imgs[i].image, cv2.COLOR_BGR2GRAY)
+        k_and_d = (sift.detectAndCompute(img_gray, None))
+        imgs[i].key_point = k_and_d[0]
+        ttl = []
+        for j in k_and_d[1]:
+            des_ele = Descriptor(j, None)
+            ttl.append(des_ele)
+        imgs[i].descriptor = ttl
+        key_des.append(k_and_d)
 
-    return img, kp, des
+    return imgs, key_des
 
-def find_nearest_node(node_list, centroid):
-    distance = []
-    for node in node_list:
-        dis = np.linalg.norm(node - centroid)
-        distance.append(dis)
-    return node_list[distance.index(min(distance))]
+def coalesce_des(key_des):
+    tmp = [key_des[i][1] for i in range(0, len(key_des)) if key_des[i][1] is not None]
+
+    ttl_len = 0
+    for i in range(len(key_des)):
+        ttl_len += len(key_des[i][0])
+
+    des_num = len(tmp[0][0])
+    des = np.zeros((ttl_len, des_num))
+
+    c = 0
+    for i in tmp:
+        for j in i:
+            des[c] = j
+            c += 1
+
+    return des
+
+def k_means(des, words):
+    # K-means algorithm parameter
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    com, lab, center = cv2.kmeans(des, words, None, criteria, 4, cv2.KMEANS_RANDOM_CENTERS)
+
+    return com, lab, center
+
+def set_labels(imgs, lab, des_num, word_bag):
+
+    idx = 0
+    for i in imgs:
+        i.histogram = np.zeros(des_num, dtype = object)
+        if i.descriptor is not None:
+            for j in i.descriptor:
+                j.label = lab[idx][0]
+                i.histogram[lab[idx][0]] += 1
+                idx += 1
+        else:
+            i.histogram = np.zeros(word_bag, dtype = np.float32)
+
+    return imgs
 
 
 if __name__ == '__main__':
-    # add_pic = os.walk('.\\data\\train\\TallBuilding')
-    visual_dict = {}
-    for file_name in os.listdir('.\\data\\train\\TallBuilding'):
-        image = cv2.imread('.\\data\\train\\TallBuilding\\' + file_name)
 
-        image, key_point, descriptor = sift_detector(image)
+    train_images = []
+    # for file_name in os.listdir('.\\data\\train\\TallBuilding'):
+    #     img = cv2.imread('.\\data\\train\\TallBuilding\\' + file_name)
+    for dir_name in os.listdir('.\\data\\train'):
+        for file_name in os.listdir('.\\data\\train\\' + dir_name):
+            img = cv2.imread('.\\data\\train\\' + dir_name + '\\' + file_name)
 
-        k_means = KMeans(n_clusters = 10, max_iter = 500, random_state = 0).fit(descriptor)
-        # visual_words = k_means.cluster_centers_
+            train_images.append(Image(img, os.path.splitext(file_name)[0]))
 
-        for centroids in k_means.cluster_centers_:
-            nearest_node = str(find_nearest_node(descriptor, centroids))
-            visual_dict[nearest_node] = visual_dict.get(nearest_node, 0) + 1
+    train_images, train_images_key_des = update_images(train_images)
 
-    print(visual_dict.values())
-    # plt.hist(visual_dict.keys())
+    descriptor = coalesce_des(train_images_key_des).astype('float32')
+
+    words_num = 150
+    compact, label, centroids = k_means(descriptor, words_num)
+
+    train_images = set_labels(train_images, label, words_num, words_num)
+
+    print(train_images[1].histogram)
